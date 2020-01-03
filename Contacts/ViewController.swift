95261a27afad9c339d4bc27c251bddda9dbd1a7c
Dylan Liu <yliu8949@gmail.com>
2020-01-03 10:35:20 -0500

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Yang Liu on 1/3/20.
+//  Copyright © 2020 Yang Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
