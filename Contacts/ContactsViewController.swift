9e5ac66d10b98dd05d495640b42a523b22d2b843
Dylan Liu <yliu8949@gmail.com>
2020-01-04 15:33:50 -0500

    Implement bottom profile information section

--- Contacts/ContactsViewController.swift
+++ Contacts/ContactsViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class ContactsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ContactsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var profileImagecollectionView: UICollectionView!
+    
+    
+    @IBOutlet weak var profileInformationCollectionView: UICollectionView!
     
     var persons: [ContactPerson] = []
     var selectedIndex: Int = 0
@@ -24,29 +29,59 @@ class ContactsViewController: UIViewController, UICollectionViewDataSource, UICo
                 self.persons = persons
             }
         }
-        // setup collection view delegates
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        // setup collection view datasource and delegate
+        profileImagecollectionView.delegate = self
+        profileImagecollectionView.dataSource = self
+        profileInformationCollectionView.delegate = self
+        profileInformationCollectionView.dataSource = self
+        profileInformationCollectionView.allowsSelection = false
+        profileInformationCollectionView.isPagingEnabled = true
     }
     
     // Mark: Implement UICollectionViewDataSource
-    @IBOutlet weak var collectionView: UICollectionView!
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return persons.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileImageCell", for: indexPath)
-        if let profileImageCell = cell as? ProfileImageCollectionViewCell {
-            profileImageCell.profileImage.image = persons[indexPath.item].profileImage
+        
+        let person = persons[indexPath.item]
+        if collectionView == profileImagecollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileImageCell", for: indexPath)
+            if let profileImageCell = cell as? ProfileImageCollectionViewCell {
+                profileImageCell.profileImage.image = person.profileImage
+            }
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileInformationCell", for: indexPath)
+            
+            if let profileInformationCell = cell as?
+                ProfileInformationCollectionViewCell {
+                profileInformationCell.contentView.frame = profileInformationCollectionView.bounds
+                
+                let attributedName = NSMutableAttributedString(string: person.first_name + " " + person.last_name);
+                
+                let boldFont = UIFont.boldSystemFont(ofSize: 24)
+                attributedName.addAttribute(.font, value: boldFont, range: NSRange(location: 0, length: person.first_name.count))
+                
+                let thinFont = UIFont(name: "HelveticaNeue-Light", size: 24) ?? UIFont.systemFont(ofSize: 24)
+                attributedName.addAttribute(.font, value: thinFont, range: NSRange(location: person.first_name.count + 1, length: person.last_name.count))
+
+                profileInformationCell.nameLabel.attributedText = attributedName
+                profileInformationCell.titleLabel.text = person.title
+                profileInformationCell.introductionLabel.text = person.introduction
+            }
+            return cell
         }
-        return cell
     }
     
     // Mark: Implement UICollectionViewDelegate
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.scrollToItem(at: indexPath, at: [.centeredHorizontally], animated: true)
+        if collectionView == profileImagecollectionView {
+            collectionView.scrollToItem(at: indexPath, at: [.centeredHorizontally], animated: true)
+            selectedIndex = indexPath.item
+        }
     }
 }
 
---
+++ Contacts/ProfileInformationCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileInformationCollectionViewCell.swift
+//  Contacts
+//
+//  Created by Yang Liu on 1/4/20.
+//  Copyright © 2020 Yang Liu. All rights reserved.
+//
+
+import UIKit
+
+class ProfileInformationCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var aboutMeLabel: UILabel!
+    @IBOutlet weak var introductionLabel: UILabel!
+}
